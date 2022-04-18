4e68dde4be4a3ec8b3239592927b0f4d9e656d42
SamuelCdaSilva <91702874+SamuelCdaSilva@users.noreply.github.com>
2022-04-18 18:02:44 -0300

    CDA

--- Atividade_Desafio2.c
+++ Atividade_Desafio2.c
@@ -40,16 +40,23 @@ int main()
 
     int opcaoMain = menu();
 
+    int local = 0;
+
+
+     
     switch (opcaoMain)
     {
     case 1:
         // CADASTRO DE ALUNOS
 
-        for (int posicao = 0; posicao < 100; posicao++)
-        {
+        //for (int posicao = 0; posicao < 100; posicao++)
+        //{
+
+            printf("\n\n em qual local voce quer cadastrar? 1 a 100: ");
+            scanf("%d", &local);
 
             printf("\n =====================");
-            printf("\n CADASTRO DE ALUNOS %d", posicao + 1);
+            printf("\n CADASTRO DE ALUNOS %d", local);
             printf("\n =====================");
 
             // Nome do aluno
@@ -75,12 +82,12 @@ int main()
             // Nota A3
             printf(" Informe sua nota na A3: ");
             scanf("%f", Alunos[posicao].A3);
-        }
+        //}
 
         break;
 
     case 2:
-        // Listar alunos
+        // Listar alunos média final
         printf("\n Nome: %s\n Email: %s\n Matricula: %d\n A1: %.1f\n A2: %.1f\n A3: %.1f\n", Alunos[100].nomes[posicao], Alunos[100].emails[posicao], Alunos[100].matriculas[posicao], Alunos[100].A1[posicao], Alunos[100].A2[posicao], Alunos[100].A3[posicao]);
 
         // Situação acadêmica
@@ -104,12 +111,14 @@ int main()
 
     case 3:
         // Excluir alunos
-        printf("\nEXCLUINDO ALUNOS \n");
+        printf("\n Nome: %s\n Email: %s\n Matricula: %d\n A1: %.1f\n A2: %.1f\n A3: %.1f\n", Alunos[100].nomes[posicao], Alunos[100].emails[posicao], Alunos[100].matriculas[posicao], Alunos[100].A1[posicao], Alunos[100].A2[posicao], Alunos[100].A3[posicao]);
         break;
 
     default:
         break;
-    }
+    } // FIM DO SWITCH
+
+
 
     return 0;
 }
